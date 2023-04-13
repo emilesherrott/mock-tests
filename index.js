a07c6b6d0ed85648e3741cf2e58b633e94b4ea36
“emilesherrott” <emilesherrott@gmail.com>
2023-04-13 15:42:41 +0100

    smashed it, 10/10

---
+++ index.js
@@ -0,0 +1,27 @@
+const addToTen = (a, b) => {
+    if(a === undefined && b ===undefined){
+        return false
+    } else {
+        return a + b === 10 ? true : false
+    }
+
+}
+
+
+const vowelCount = (str) => {
+ if(str === ""){
+    return "Empty string passed, no vowels present"
+ } else {
+    let vowels = ['a','e','i','o','u']
+    let letters = str.split("")
+    let count = letters.filter(letter =>  vowels.includes(letter.toLowerCase())).length
+    return count
+ }
+}
+
+vowelCount("Victoria Coach Station")
+
+module.exports = {
+    addsToTen, 
+    vowelCount
+}
