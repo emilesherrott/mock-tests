6e3c3ef0098fcc883deac78c264c227a522e835d
“emilesherrott” <emilesherrott@gmail.com>
2023-04-13 15:43:06 +0100

    smashed it, 10/10

--- index.test.js
+++ index.test.js
@@ -4,39 +4,39 @@ const { addsToTen, vowelCount } = require("./index")
 
 describe("addsToTen", () => {
 
-    // it("Is a function", () => {
-    //     expect(addsToTen).toBeDefined();
-    //     expect(addsToTen instanceof Function).toEqual(true);
-    // })
+    it("Is a function", () => {
+        expect(addsToTen).toBeDefined();
+        expect(addsToTen instanceof Function).toEqual(true);
+    })
 
-    // it("Returns false when passed no values", () => {
-    //     const result = addsToTen()
-    //     expect(typeof result).toBe("boolean")
-    //     expect(Boolean(result)).toBe(false);
-    // })
+    it("Returns false when passed no values", () => {
+        const result = addsToTen()
+        expect(typeof result).toBe("boolean")
+        expect(Boolean(result)).toBe(false);
+    })
 
-    // each([
-    //     [ true, 9, 1],
-    //     [ false, 11, 1],
-    //     [ false, 10, 5],
-    // ]).test(`Returns %s when passed %s and %s`, (expected, inputA, inputB) => {
-    //     expect(addsToTen(inputA, inputB)).toEqual(expected);
-    // })
+    each([
+        [ true, 9, 1],
+        [ false, 11, 1],
+        [ false, 10, 5],
+    ]).test(`Returns %s when passed %s and %s`, (expected, inputA, inputB) => {
+        expect(addsToTen(inputA, inputB)).toEqual(expected);
+    })
 
 })
 
 describe("vowelCount", () => {
 
-    // it("Is a function", () => {
-    //     expect(vowelCount).toBeDefined();
-    //     expect(vowelCount instanceof Function).toEqual(true);
-    // })
+    it("Is a function", () => {
+        expect(vowelCount).toBeDefined();
+        expect(vowelCount instanceof Function).toEqual(true);
+    })
 
-    // it("Returns a descriptive message (string) when passed an empty string", () => {
-    //     const result = vowelCount("")
-    //     expect(typeof result).toBe("string");
-    //     expect(result).toBe("Empty string passed, no vowels present");
-    // })
+    it("Returns a descriptive message (string) when passed an empty string", () => {
+        const result = vowelCount("")
+        expect(typeof result).toBe("string");
+        expect(result).toBe("Empty string passed, no vowels present");
+    })
 
     each([
         [9, "Victoria Coach Station"],
